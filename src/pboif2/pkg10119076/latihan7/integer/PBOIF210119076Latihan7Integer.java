d0ed2cf933c71869abae093687b35cd599a3d7d3
ACER <ACER@DESKTOP-JMQ2UKH>
2020-10-17 17:28:23 +0700

    PBOIF2-10119076-Latihan7-Integer

---
+++ src/pboif2/pkg10119076/latihan7/integer/PBOIF210119076Latihan7Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119076.latihan7.integer;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Sulthon Naufal Akmal
+ * KELAS    : IF2
+ * NIM      : 10119076
+ * Deskripsi Program : integer
+ */
+public class PBOIF210119076Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
